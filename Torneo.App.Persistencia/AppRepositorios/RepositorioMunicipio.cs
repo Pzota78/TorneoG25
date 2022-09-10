2d5aa82e316bac7cd586a4528cf665b4fe9bc693
Pzota78 <pablozota@gmail.com>
2022-09-10 09:23:04 -0500

    Repositorio Municipio2

--- Torneo.App.Persistencia/AppRepositorios/RepositorioMunicipio.cs
+++ Torneo.App.Persistencia/AppRepositorios/RepositorioMunicipio.cs
@@ -4,5 +4,13 @@ namespace Torneo.App.Persistencia
 {
     public class RepositorioMunicipio : IRepositorioMunicipio
     {
+        private readonly DataContext dataContext = new DataContext();
+
+        public Municipio AddMunicipio (Municipio municipio)
+        {
+            var municipioInsertado = dataContext.Municipios.Add (municipio);
+            _dataContext SaveChanges();
+            return municipioInsertado.Entity;
+}
     }
 }
