e376b8396a4cedb03637c031baebc6c934696f29
Pzota78 <pablozota@gmail.com>
2022-09-11 14:04:00 -0500

    Menu DT Consola

--- Torneo.App.Consola/Program.cs
+++ Torneo.App.Consola/Program.cs
@@ -4,36 +4,42 @@ namespace Torneo.App.Consola
 {
     class Program
     {
-       private static IRepositorioMunicipio _repoMunicipio = new RepositorioMunicipio();
-        
+        private static IRepositorioDT _repoDT = new RepositorioDT();
         static void Main(string[] args)
         {
             int opcion = 0;
-            do
+            do 
             {
-                Console.WriteLine("1 Insertar Municipio");
+                Console.WriteLine("2 Insertar director tecnico");
                 Console.WriteLine("0 Salir");
                 opcion = Int32.Parse(Console.ReadLine());
                 switch (opcion)
                 {
-                    case 1:
-                        AddMunicipio();
+                   case 2:
+                        AddDT(); 
                         break;
                 }
 
             } while (opcion != 0);
-            
+           
         }
 
-        private static void AddMunicipio()
+        private static void AddDT()
         {
-            Console.WriteLine("Ingrese el nombre del municipio");
+            Console.WriteLine("Ingrese el nombre del DT"); 
             string nombre = Console.ReadLine();
-            var municipio = new Municipios
-            { 
+            Console.WriteLine("Ingrese el documento del DT"); 
+            string documento = Console.ReadLine();
+            Console.WriteLine("Ingrese el telefono del director tecnico");
+            string telefono = Console.ReadLine();
+            var directorTecnico = new DirectorTecnico
+            
+            {
                 Nombre = nombre,
+                Documento = documento,
+                Telefono = telefono,
             };
-            _repoMunicipio.AddMunicipio(municipio);
+            _repoDT.AddDT(directorTecnico);
         }
     }
 }
