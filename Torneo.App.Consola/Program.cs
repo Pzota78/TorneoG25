fc030e844a95abbe9f2450bcd523d0f0464d8a87
Pzota78 <pablozota@gmail.com>
2022-09-11 19:38:14 -0500

    Consola AddEquipo

--- Torneo.App.Consola/Program.cs
+++ Torneo.App.Consola/Program.cs
@@ -7,6 +7,7 @@ namespace Torneo.App.Consola
 
         private static IRepositorioMunicipio _repoMunicipio = new RepositorioMunicipio();
         private static IRepositorioDT _repoDT = new RepositorioDT();
+        private static IRepositorioEquipo _repoEquipo = new RepositorioEquipo();
         static void Main(string[] args)
         {
             int opcion = 0;
@@ -14,6 +15,7 @@ namespace Torneo.App.Consola
             {
                 Console.WriteLine("1 Insertar Municipio");
                 Console.WriteLine("2 Insertar director tecnico");
+                Console.WriteLine("3 Insertar Equipo");
                 Console.WriteLine("0 Salir");
                 opcion = Int32.Parse(Console.ReadLine());
                 switch (opcion)
@@ -23,13 +25,16 @@ namespace Torneo.App.Consola
                         AddMunicipio();
                         break;
 
-                   case 2:
+                    case 2:
                         AddDT();
+                        break;
+                    case 3:
+                        AddEquipo();
                         break;
                 }
             } while (opcion != 0);
 
-            
+
         }
 
         private static void AddMunicipio()
@@ -37,31 +42,45 @@ namespace Torneo.App.Consola
             Console.WriteLine("Ingrese el nombre del municipio");
             string nombre = Console.ReadLine();
             var municipio = new Municipio
-            { 
+            {
                 Nombre = nombre,
             };
             _repoMunicipio.AddMunicipio(municipio);
 
-           
+
         }
 
         private static void AddDT()
         {
-            Console.WriteLine("Ingrese el nombre del DT"); 
+            Console.WriteLine("Ingrese el nombre del DT");
             string nombre = Console.ReadLine();
-            Console.WriteLine("Ingrese el documento del DT"); 
+            Console.WriteLine("Ingrese el documento del DT");
             string documento = Console.ReadLine();
             Console.WriteLine("Ingrese el telefono del director tecnico");
             string telefono = Console.ReadLine();
             var directorTecnico = new DirectorTecnico
-            
+
             {
                 Nombre = nombre,
                 Documento = documento,
                 Telefono = telefono,
             };
             _repoDT.AddDT(directorTecnico);
+        }
+        private static void AddEquipo()
+        {
+            Console.WriteLine("Ingrese el nombre del equipo");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el id del municipio");
+            int idMunicipio = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el id del DT");
+            int idDT = Int32.Parse(Console.ReadLine());
 
+            var equipo = new Equipo
+            {
+                Nombre = nombre,
+            };
+            _repoEquipo.AddEquipo(equipo, idMunicipio, idDT);
         }
     }
 }
