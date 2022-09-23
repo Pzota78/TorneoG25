d0e02b318cf00bb0f47b784a207981e78934c870
Eduard Idrobo <idroboeduard@gmail.com>
2022-09-23 03:48:39 -0500

    CrearJugadores

--- Torneo.App.Frontend/Pages/Jugadores/Create.cshtml.cs
+++ Torneo.App.Frontend/Pages/Jugadores/Create.cshtml.cs
@@ -1,12 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Torneo.App.Dominio;
+using Torneo.App.Persistencia;
 
 namespace Torneo.App.Frontend.Pages.Jugadores
 {
     public class CreateModel : PageModel
     {
+        private readonly IRepositorioJugador _repoJugador;
+        private readonly IRepositorioPosicion _repoPosicion;
+        private readonly IRepositorioEquipo _repoEquipo;
+
+        public Jugador jugador { get; set; }
+        public IEnumerable<Equipo> equipo { get; set; }
+        public IEnumerable<Posicion> posicion { get; set; }
+
+        public CreateModel(IRepositorioJugador repoJugador, IRepositorioEquipo repoEquipo, IRepositorioPosicion repoPosicion)
+        {
+            _repoJugador = repoJugador;
+            _repoEquipo = repoEquipo;
+            _repoPosicion = repoPosicion;
+        }
         public void OnGet()
         {
+            jugador = new Jugador();
+            equipo = _repoEquipo.GetAllEquipos();
+            posicion = _repoPosicion.GetAllPosicion();
+        }
+        public IActionResult OnPost(Jugador jugador, int idPosicion, int idEquipo)
+        {
+            _repoJugador.AddJugador(jugador, idPosicion, idEquipo);
+            return RedirectToPage("Index");
         }
     }
 }
