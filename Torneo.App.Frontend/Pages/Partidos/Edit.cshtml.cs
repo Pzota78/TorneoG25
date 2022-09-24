139d96b996df6a90f36242c1cb6d78be4a87de24
Eduard Idrobo <idroboeduard@gmail.com>
2022-09-24 02:21:07 -0500

    EditarPartido

---
+++ Torneo.App.Frontend/Pages/Partidos/Edit.cshtml.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Torneo.App.Dominio;
+using Torneo.App.Persistencia;
+
+namespace Torneo.App.Frontend.Pages.Partidos
+{
+    public class EditModel : PageModel
+    {
+        private readonly IRepositorioPartido _repoPartido;
+        private readonly IRepositorioEquipo _repoEquipoLocal;
+        private readonly IRepositorioEquipo _repoEquipoVisitante;
+
+        public Partido partido { get; set; }
+        public IEnumerable<Equipo> equipoLocal { get; set; }
+        public IEnumerable<Equipo> equipoVisitante { get; set; }
+
+        public EditModel(IRepositorioPartido repoPartido, IRepositorioEquipo repoEquipoLocal, IRepositorioEquipo repoEquipoVisitante)
+        {
+            _repoPartido = repoPartido;
+            _repoEquipoLocal = repoEquipoLocal;
+            _repoEquipoVisitante = repoEquipoVisitante;
+        }
+        public IActionResult OnGet(int id)
+        {
+            partido = _repoPartido.GetPartido(id);
+            equipoLocal = _repoEquipoLocal.GetAllEquipos();
+            equipoVisitante = _repoEquipoVisitante.GetAllEquipos();
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+            else{
+                return Page();
+            }
+            
+        }
+        public IActionResult OnPost(Partido partido, int idLocal, int idVisitante)
+        {
+            _repoPartido.UpdatePartido(partido, idLocal, idVisitante);
+            return RedirectToPage("Index");
+        }
+    }
+}
